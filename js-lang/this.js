77dd0e9f174f28c6ad537536cf909f954b512f54
0xlakshan <lakshanperera.dev@gmail.com>
2022-05-21 22:02:14 +0530

    update

--- js-lang/this.js
+++ js-lang/this.js
@@ -44,11 +44,58 @@
 
 // new Example();
 
+// var a = 'Global';
+// function whatsThis() {
+//     console.log(this.a);
+// }
+// whatsThis();
+
+// function myFunction() {
+//     console.log(this.x, this.y);
+//     console.log(this);
+// }
+// const myObj = {
+//     x: 'valueOfX',
+//     y: 'valueOfY'
+// }
+// myFunction.call(myObj);
+// const bound = myFunction.bind(myObj);
+// bound();
+// myFunction.apply(myObj, [1, 2])
+
+
 // obj = {name: 'myobj'};
 // console.log(obj);
 // console.log(Object.getPrototypeOf(obj));
 
-// [ ] - Learn Object.create
-// Higher Order Functions
-// Hoisting
-// Clousure
+/* object.create */
+// const target = { a:1, b:2, c:3, d:7 };
+// const source = { a:1, b:2, d:8 };
+// const newObj = Object.assign(target, source);
+// console.log(newObj);
+
+/* object.call */
+// function Product(name, price) {
+//     this.name = name;
+//     this.price = price;
+// }
+// function Food(name, price) {
+//     Product.call(this, name, price)
+//     this.category = 'food';
+// }
+// console.log(new Food('cheese', 5).name);
+
+// const arr = [1, 2, 3, 4, 5];
+// arr.push.apply(arr, [11, 22, 33]);
+// console.log(arr);
+
+/* bind */
+// const obj = {
+//     value: 'x',
+//     method: () => this.value
+// }
+// function valueTeller() {
+//     console.log(this.value);
+// }
+// const boundFunc = valueTeller.bind(obj);
+// boundFunc();
